2c477b1721ec64b77b1d674beb2ba3140289a7bc
Tsu <lucanahtsu@gmail.com>
2023-07-31 11:26:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App
