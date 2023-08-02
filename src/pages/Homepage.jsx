6e126ade8e0530fd33b60ee427d9137f254a46c9
Tsu <lucanahtsu@gmail.com>
2023-08-02 14:26:59 -0300

    Add parallax animation

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import MainLayout from '../layouts/MainLayout'
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+import originalImage from '../assets/originalbg.png';
+import roadCutoutImage from '../assets/roadbg.png';
+
 function Homepage() {
     return (
         <MainLayout>
-            <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">MyTinerary</h1>
-                        <p className="mb-5">Find your perfect trip, designed by insiders who know and love their cities</p>
-                        <button className="btn btn-primary">Get Started</button>
+            <div className="hero min-h-screen relative overflow-hidden">
+                <div
+                    className="absolute inset-0 w-full h-full bg-cover bg-center z-[-2]"
+                    style={{ backgroundImage: `url(${originalImage})` }}
+                ></div>
+
+                <div className="absolute inset-0 w-full z-1">
+                    <img src={roadCutoutImage} alt="Road Cutout" draggable="false" className="w-full h-full object-cover object-center pointer-events-none" />
+                </div>
+
+                {/* Hero Content */}
+                <div className="hero-content text-center text-neutral-content relative -z-1 fade-in-bottom">
+                    <div className="max-w-xl mx-auto">
+                        <div className="mt-[-350px] animate-fadeIn">
+                            <h1 className="mb-5 font-black text-5xl sm:text-8xl"><span className='text-primary'>My</span><span className='text-secondary'>Tinerary</span></h1>
+                            <p className="mb-5 text-xl font-bold text-base-100">Find your perfect trip, designed by insiders who know and love their cities.</p>
+                            <button className="btn btn-wide">Get Started</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </MainLayout>
-    )
+    );
 }
 
-export default Homepage
+export default Homepage;
