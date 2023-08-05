0805684203394a99e0529f84d9f49958ff0247ec
Tsu <lucanahtsu@gmail.com>
2023-08-05 13:14:49 -0300

    Add City and Country to the carousel images

--- src/components/PopularMyTineraries.jsx
+++ src/components/PopularMyTineraries.jsx
@@ -9,13 +9,28 @@ const Carousel = ({ currentSlide }) => {
                 {Array.from({ length: imagesPerSlide }).map((_, index) => {
                     const imageIndex = currentSlide * imagesPerSlide + index + 1;
                     return (
-                        <div key={index} className="carousel-slide w-1/2 md:w-1/4">
+                        <a key={index} className="carousel-slide w-1/2 md:w-1/4 cursor-pointer relative group">
                             <img
                                 src={`https://via.placeholder.com/300?text=Slide ${currentSlide + 1} - Image ${imageIndex}`}
                                 alt={`Slide${currentSlide + 1}-Image${imageIndex}`}
-                                className="w-full h-full object-cover"
+                                className="w-full h-full object-cover rounded-sm transition-all brightness-50 group-hover:brightness-100"
                             />
-                        </div>
+                            <div className="absolute bottom-0 left-0 p-2 text-white group-hover:opacity-0 transition-opacity">
+                                <h3 className="text-lg md:text-2xl font-semibold">City {imageIndex}</h3>
+                                <div className='flex items-center'>
+                                    <svg className="h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                                        <g id="SVGRepo_iconCarrier">
+                                            <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                            <path d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                        </g>
+                                    </svg>
+                                    <h4 className="text-xs md:text-sm">Country {imageIndex}</h4>
+                                </div>
+                            </div>
+                        </a>
+
                     );
                 })}
             </div>
