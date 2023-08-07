7c21f08d65913af67e8160f8a537a9313998facc
Tsu <lucanahtsu@gmail.com>
2023-08-07 12:22:36 -0300

    Add Work in Progress page and 404 error page

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Cities() {
     return (
         <>
-            <div>Cities</div>
+            <section id="cities" className='text-3xl lg:text-7xl font-black text-white bg-neutral min-h-screen flex items-center justify-center'>
+                ⚒️Work in progress⚒️
+            </section>
         </>
     )
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function PageNotFound() {
     return (
-        <div>PageNotFound</div>
+        <section id="pageNotFound" className='text-3xl lg:text-7xl font-black text-white bg-neutral min-h-screen flex flex-col items-center justify-center'>
+            🚫404 - Not found🚫
+            <Link to="/">
+                <button className='btn btn-accent btn-outline btn-wide m-20'>Go to home</button>
+            </Link>
+        </section>
     )
 }
 
