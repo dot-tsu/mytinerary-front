b6c051a83313c1821b2879a93eae902bbf423c1a
Luca Di Marco <lucanahtsu@gmail.com>
2023-08-17 12:55:14 -0300

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  homepage: "https://tsuramii.github.io/myTinerary-lucaDiMarco/"
+  base: "/myTinerary-lucaDiMarco"
 })
