458a055b92fc6d66d4cf9da11af3df81878fda3c
Luca Di Marco <lucanahtsu@gmail.com>
2023-08-17 09:33:35 -0300

    Update Home Component: Refactor, Improved Accessibility, and Image Rename

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 import originalImage from '../assets/originalbg.jpg';
-import cuttedImage from '../assets/cuttedbg.png';
-import { Link } from 'react-router-dom'
+import cutoutImage from '../assets/cuttedbg.png';
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
     return (
         <section id="home" className="hero min-h-screen relative overflow-hidden flex">
-            {/* Parallax background */}
             <img
                 src={originalImage}
                 alt="Person in landscape"
@@ -14,7 +13,6 @@ function Home() {
                 className="w-full h-full object-cover object-center pointer-events-none -z-50 fixed brightness-50"
             />
 
-            {/* SVG Shape */}
             <svg
                 className="absolute bottom-0 left-0 w-full text-white -z-40"
                 viewBox="0 0 720 320"
@@ -26,10 +24,9 @@ function Home() {
                 ></path>
             </svg>
 
-            {/* Hero Content */}
             <div className="hero-content h-full z-10 flex flex-col w-4/5 md:w-3/5 text-white">
                 <h1 className="text-5xl md:text-7xl xl:text-9xl font-bold">
-                    My<span className="">Tinerary</span>
+                    My<span className="text-accent">Tinerary</span>
                 </h1>
                 <p className="text-sm xl:text-xl font-thin md:mx-10">
                     Find your perfect trip, designed by insiders who know and love their cities.
@@ -41,15 +38,14 @@ function Home() {
                 </Link>
             </div>
 
-            {/* Person Cutout */}
             <img
-                src={cuttedImage}
+                src={cutoutImage}
                 alt="Person Cutout"
                 draggable="false"
                 className="inset w-full h-full object-cover object-center pointer-events-none fixed -z-30 brightness-50"
             />
         </section>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
