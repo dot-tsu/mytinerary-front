f1425c33bf8f022e80a728212472d2d552359664
Luca Di Marco <lucanahtsu@gmail.com>
2023-08-18 13:30:27 -0300

    Make background fixed

--- src/components/CityHeader.jsx
+++ src/components/CityHeader.jsx
@@ -32,7 +32,7 @@ const CityHeader = () => {
   }, [headerPlaces]);
 
   return (
-    <div className="relative flex flex-col justify-center items-center min-h-[40vh] bg-cover bg-left" style={{ backgroundImage: headerPlaces.length === 0 ? "" : `url('${headerPlaces[currentImageIndex].image_url}')` }}>
+    <div className="relative flex flex-col justify-center items-center min-h-[40vh] bg-fixed bg-cover bg-bottom" style={{ backgroundImage: headerPlaces.length === 0 ? "" : `url('${headerPlaces[currentImageIndex].image_url}')` }}>
       <div className={`absolute inset-0 bg-black transition-opacity duration-300 ${isChanging ? "opacity-75" : "opacity-50"}`}></div>
       <h1 className='text-white text-5xl md:text-7xl font-bold relative z-10'>Cities</h1>
       <h3 className='text-white md:text-xl font-light m-3 relative z-10'>
