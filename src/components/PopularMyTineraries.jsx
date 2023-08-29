c73c925c5bd34029841074b7ce956e1e152fdd4e
Luca Di Marco <lucanahtsu@gmail.com>
2023-08-29 10:29:55 -0300

    Use PlaceCard.jsx for the cards

--- src/components/PopularMyTineraries.jsx
+++ src/components/PopularMyTineraries.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+
+import PlaceCard from './PlaceCard.jsx';
+import CardSkeleton from './CardSkeleton.jsx';
 
 const Carousel = ({ currentSlide, setCurrentSlide, filteredPlaces }) => {
     const imagesPerSlide = 4;
@@ -16,44 +18,27 @@ const Carousel = ({ currentSlide, setCurrentSlide, filteredPlaces }) => {
     }, [currentSlide]);
 
     return (
-        <div className="relative">
-            <div className="h-fit justify-center align-middle grid grid-cols-1 sm:grid-cols-2 gap-2 md:gap-4">
-                {filteredPlaces.length === 0 ? (
-                    <p>Loading...</p>
-                ) : (
-                    filteredPlaces
-                        .slice(currentSlide * imagesPerSlide, (currentSlide + 1) * imagesPerSlide)
-                        .map((place) => {
-                            return (
-                                <Link to={`/cities/${place._id}`} key={place._id}>
-                                <div className="relative group">
-                                    <img
-                                        src={place.image_url}
-                                        alt={place.city + ", " + place.country}
-                                        className="w-full h-32 md:h-56 lg:h-64 object-cover rounded-md transition-all brightness-50 group-hover:brightness-100"
-                                    />
-                                    <div className="absolute bottom-0 left-0 p-2 text-white group-hover:opacity-0 transition-opacity">
-                                        <h3 className="text-lg md:text-2xl font-semibold">{place.city}</h3>
-                                        <div className='flex places-center'>
-                                            <svg className="h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                                                <g id="SVGRepo_iconCarrier">
-                                                    <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                    <path d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                </g>
-                                            </svg>
-                                            <h4 className="text-xs md:text-sm">{place.country}</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                                </Link>
-                            );
-                        })
-                )}
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-8">
+            {filteredPlaces.length === 0 ? (
+                <>
+                    <CardSkeleton />
+                    <CardSkeleton />
+                    <CardSkeleton />
+                    <CardSkeleton />
+                </>
+            ) : (
+                filteredPlaces
+                    .slice(currentSlide * imagesPerSlide, (currentSlide + 1) * imagesPerSlide)
+                    .map((place) => {
+                        return (
+                            <>
+                                <PlaceCard place={place} key={place._id} />
+                            </>
+                        );
+                    })
+            )}
         </div>
-    );    
+    );
 };
 
 const PopularMyTineraries = () => {
@@ -74,7 +59,7 @@ const PopularMyTineraries = () => {
     }, []);
 
     const totalSlides = Math.ceil(popularPlaces.length / 4);
-    
+
     const prevSlide = () => {
         setCurrentSlide(currentSlideIndex => (currentSlideIndex > 0 ? currentSlideIndex - 1 : totalSlides - 1));
     };
@@ -84,25 +69,25 @@ const PopularMyTineraries = () => {
     };
 
     return (
-        <section id="popularMyTineraries" className='min-h-screen bg-gradient-to-b from-white to-base-100 flex justify-center flex-col py-5'>
-            <div className='mx-20'>
-                <h1 className='font-bold text-4xl text-primary mb-5 mt-10 text-center'>Explore your next favorite destination</h1>
-                <p className='text-center font-light text-md sm:text-lg mb-10'>Discover amazing places around the world and plan your next adventure with MyTinerary.</p>
-                <div className='card bg-white shadow-lg'>
-                    <div className='m-3'>
-                        <div className='grid grid-cols-3'>
-                            <h2 className='card-title mx-5 col-span-2 flex font-light text-secondary text-sm md:text-2xl'>Popular MyTineraries</h2>
-                            <div className='col-span-1 flex justify-end places-center gap-2 m-2'>
-                                <button className="btn btn-sm btn-ghost btn-circle" onClick={prevSlide}>
-                                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.071 5L7.70708 11.364C7.31656 11.7545 7.31656 12.3877 7.70708 12.7782L14.071 19.1421" stroke="#000000" strokeLinecap="round"></path> </g></svg>
-                                </button>
-                                <button className="btn btn-sm btn-ghost btn-circle" onClick={nextSlide}>
-                                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.92896 4.85785L16.2929 11.2218C16.6834 11.6123 16.6834 12.2455 16.2929 12.636L9.92896 19" stroke="#000000" strokeLinecap="round"></path> </g></svg>
-                                </button>
-                            </div>
+        <section id="popularMyTineraries" className='min-h-screen bg-gradient-to-b from-white to-base-100 flex justify-center flex-col items-center'>
+            <h1 className='font-bold text-4xl text-primary mb-5 mt-10 text-center'>Explore your next favorite destination</h1>
+            <p className='text-center font-light text-md sm:text-lg mb-10'>Discover amazing places around the world and plan your next adventure with MyTinerary.</p>
+            <div className='bg-white shadow-lg rounded-lg'>
+                <div className='m-3'>
+                    <div className='grid grid-cols-3'>
+                        <h2 className='mx-5 col-span-2 flex font-light text-secondary text-sm md:text-2xl'>Popular MyTineraries</h2>
+                        <div className='col-span-1 flex justify-end places-center gap-2 m-2'>
+                            <button className="btn btn-sm btn-ghost btn-circle" onClick={prevSlide}>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.071 5L7.70708 11.364C7.31656 11.7545 7.31656 12.3877 7.70708 12.7782L14.071 19.1421" stroke="#000000" strokeLinecap="round"></path> </g></svg>
+                            </button>
+                            <button className="btn btn-sm btn-ghost btn-circle" onClick={nextSlide}>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.92896 4.85785L16.2929 11.2218C16.6834 11.6123 16.6834 12.2455 16.2929 12.636L9.92896 19" stroke="#000000" strokeLinecap="round"></path> </g></svg>
+                            </button>
                         </div>
-                        <Carousel currentSlide={currentSlide} setCurrentSlide={setCurrentSlide} filteredPlaces={popularPlaces} />
                     </div>
+                </div>
+                <div>
+                    <Carousel currentSlide={currentSlide} setCurrentSlide={setCurrentSlide} filteredPlaces={popularPlaces} />
                 </div>
             </div>
         </section>
