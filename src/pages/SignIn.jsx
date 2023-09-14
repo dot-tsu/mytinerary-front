4e4e229b2d5da38dd2809c3c19c7a094e8da36cc
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-13 22:31:00 -0300

    Add an alert with the error

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -34,46 +34,61 @@ const SignIn = () => {
     };
 
     return (
-        <section id="signin" className='flex min-h-screen items-center justify-center max-w-xl py-28'>
-            <div className="bg-primary w-2/3 rounded-3xl shadow-xl">
-                <h2 className="text-4xl text-white text-center p-5 font-light">Login</h2>
-                <div className='bg-white p-8 rounded-3xl rounded-tr-none'>
-                    <form onSubmit={handleSubmit} className='form-control'>
-                        <label htmlFor="email"><span className="label-text">What is your email?</span></label>
-                        <input
-                            type="email"
-                            className="input input-sm input-bordered w-full max-w-xs bg-white"
-                            id="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleInputChange}
-                            required
-                        />
-
-                        <label htmlFor="password"><span className="label-text">What is your password?</span></label>
-                        <input
-                            type="password"
-                            className="input input-sm input-bordered w-full max-w-xs bg-white"
-                            id="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleInputChange}
-                            required
-                        />
-                        <a className='link link-hover place-self-end font-semibold mt-2'>Forgot your password?</a>
-                        {error && <p className='text-red-500'>{error}</p>}
-                        <button type="submit" className='btn btn-sm btn-primary mt-5'>Login</button>
-                    </form>
-
-                    <div className='divider'>Or login with</div>
-
-                    <div>
-                        <GoogleSignInButton />
+        <>
+            {error && (
+                error === 'Email not registered 😿' ? (
+                    <div className="alert alert-warning absolute right-10 top-10 z-50 max-w-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                        <span>Email not registered<br /> Would you like to <Link to='/signup' className='link link-hover font-semibold'>Sign up now</Link>?</span>
                     </div>
-                    <p className='text-center pt-5'>Don&apos;t have an account? <Link to='/signup' className='link link-hover font-semibold'>Sign up now</Link></p>
+                ) : (
+                    <div className="alert alert-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>{error}</span>
+                    </div>
+                )
+            )}            <section id="signin" className='flex min-h-screen items-center justify-center max-w-xl py-28'>
+
+                <div className="bg-primary w-2/3 rounded-3xl shadow-xl">
+                    <h2 className="text-4xl text-white text-center p-5 font-light">Login</h2>
+                    <div className='bg-white p-8 rounded-3xl rounded-tr-none'>
+                        <form onSubmit={handleSubmit} className='form-control'>
+                            <label htmlFor="email"><span className="label-text">What is your email?</span></label>
+                            <input
+                                type="email"
+                                className="input input-sm input-bordered w-full max-w-xs bg-white"
+                                id="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleInputChange}
+                                required
+                            />
+
+                            <label htmlFor="password"><span className="label-text">What is your password?</span></label>
+                            <input
+                                type="password"
+                                className="input input-sm input-bordered w-full max-w-xs bg-white"
+                                id="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <a className='link link-hover place-self-end font-semibold mt-2'>Forgot your password?</a>
+
+                            <button type="submit" className='btn btn-sm btn-primary mt-5'>Login</button>
+                        </form>
+
+                        <div className='divider'>Or login with</div>
+
+                        <div>
+                            <GoogleSignInButton />
+                        </div>
+                        <p className='text-center pt-5'>Don&apos;t have an account? <Link to='/signup' className='link link-hover font-semibold'>Sign up now</Link></p>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
