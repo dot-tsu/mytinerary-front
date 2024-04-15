f04807e4a4befe037f2c6db891e3768cba627af5
tsuramii <lucanahtsu@gmail.com>
2024-04-15 02:35:56 -0300

    Update links for new deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://tsuramii.github.io/myTinerary-lucaDiMarco/"
-})
+  base: "https://tsuramii.github.io/mytinerary-front/",
+});
